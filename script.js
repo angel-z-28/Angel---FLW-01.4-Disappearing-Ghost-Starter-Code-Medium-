2cd995e1ee5c16d982800f14b3ec4bbc6c0c652a
angel-z-28 <105798706+angel-z-28@users.noreply.github.com>
2022-10-27 23:19:59 +0000

    I finished the hide me button, show me button, update image button, but still need to do the send message and name me buttons

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+//all selector variables
+let p = document.createElement("p");
+let h1 = document.querySelector(".header");
+let input = document.querySelector(".input");
+let userInput;
+let ghostImg = document.querySelector(".ghost-image");
+let newImg = document.querySelector(".new-image");
+let hideButton = document.querySelector(".hide-button");
+let showButton = document.querySelector(".show-button");
+let updateImg = document.querySelector(".update-img-button");
+let messageButton = document.querySelector(".message-button");
+let message = document.querySelector(".message");
+let nameButton = document.querySelector(".name-button");
 
+
+//create an onclick event for the Hide Me button
+hideButton.onclick = (function (){  
+   userInput = input.value;
+  ghostImg.style.display="none";
+  newImg.style.display="none";
+});
+
+//create an onclick event for the Show Me button
+showButton.onclick = (function (){  
+   userInput = input.value;
+  ghostImg.style.display="";
+  newImg.style.display="none";
+});
+
+
+//create an onclick event for the Update Img button
+newImg.style.display="none";
+updateImg.onclick = (function (){  
+   userInput = input.value;
+  ghostImg.style.display="none";
+  newImg.style.display="";
+});
+
+
+//create an onclick event for the Send Message button
+//update the userInput variable by saving the value of the input
+//use the insertAdjacentHTML method to append the userInput in a <p> tag
+messageButton.onclick = (function (){  
+
+});
+
+
+//create an onclick event for the Name Me button
+//update the userInput variable by saving the value of the input
+//use the innerHTML method to update the h1 tag
